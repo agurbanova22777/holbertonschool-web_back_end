c9c8c46b595812001b0759325101b4f0ddcefbb5
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-05-31 21:48:50 +0400

    did tasks 0-1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module defines a coroutine that collects values from an async generator."""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers from async_generator and return them as a list."""
+    return [value async for value in async_generator()]

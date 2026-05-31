f63b651f573229251248b2dae46fd1009f999614
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-05-31 21:57:02 +0400

    fixed 1 and 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""This module defines a coroutine that collects values from an async generator."""
+"""This module defines a coroutine that collects values from an async generator.
+"""
 
 from typing import List
 
@@ -8,6 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers from async_generator and return them."""
-    return [
-        value async for value in async_generator()
-    ]
+    return [value async for value in async_generator()]
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module measures the runtime of running async comprehensions in parallel."""
+"""This module measures the runtime of async comprehensions run in parallel."""
 
 import asyncio
 import time
@@ -8,13 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Run async_comprehension four times in parallel and return total runtime."""
+    """Execute async_comprehension four times in parallel and return runtime."""
     start: float = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
-    total: float = time.time() - start
-    return total
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.time() - start

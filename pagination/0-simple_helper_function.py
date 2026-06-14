9721ea81f022e26d54aa9d9a0863973ee61bba90
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-14 23:11:40 +0400

    removed whitespaces

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """module for pagination helper function"""
 
 
-def index_range(page : int, page_size: int) -> tuple :
+def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two
     """
     start = (page - 1) * page_size

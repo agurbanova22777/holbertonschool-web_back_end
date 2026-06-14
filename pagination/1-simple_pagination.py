52b498dfd7958b7529eb603b09d6f479a300ba75
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-14 23:37:21 +0400

    indented the function

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -34,9 +34,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """Return a page of the dataset."""
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
+        """Return a page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-    start, end = index_range(page, page_size)
-    return self.dataset()[start:end]
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]

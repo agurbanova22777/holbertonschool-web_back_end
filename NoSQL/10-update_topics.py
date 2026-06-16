d042573e0386969fac8b6b3f1639a35047768afc
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-16 16:35:29 +0400

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module for mongodb"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """change all topics of a school"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )

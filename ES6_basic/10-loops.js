2e085af739223556efdfcff84868fc2c1fcfa228
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-24 14:54:04 +0400

    added new line

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [idx, value] of array.entries()) {
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}

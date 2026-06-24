d45fa92a514ef88c1808228180148f250c7cec38
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-24 16:58:35 +0400

    task 12

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const obj = {
+    const employeesList = {
         [departmentName]: employees,
     };
-    return obj;
+    return employeesList;
 }

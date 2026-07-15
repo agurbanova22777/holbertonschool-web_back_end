93a475e785721c8dcccf1d5062d65190c4b93ca9
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-07-15 15:32:30 +0400

    task 1

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success){
+    if(success === true) {
+        return Promise.resolve({
+            status: 200,
+            body: "Success"
+        })
+    }
+    else {
+        return Promise.reject(new Error("The fake API is not working currently"));
+    }
+}

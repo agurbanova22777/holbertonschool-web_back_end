bfb4efa8ba6809d98d67b1da649771f594321128
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-07-22 16:56:52 +0400

    changed printing method:

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-function stdin() {
-  console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
-  })
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-  process.on('SIGINT', () => {
-    console.log("This important software is now closing");
-  })
-}
-
+process.on('SIGINT', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
 
 module.exports = stdin();

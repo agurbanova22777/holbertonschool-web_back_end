bd515aa16004a354908efb960a480b5088c5c51b
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-07-23 14:17:33 +0400

    task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+function countStudents(path){
+  let data;
+  try{
+    data = fs.readFileSync(path, 'utf8');
+  }
+  catch{
+    throw new Error('Cannot load the database');
+  }
+  const lines = data.split('\n');
+  const students = lines.filter(line => line !== '');
+  const studentData = students.slice(1);
+
+  console.log(`Number of students: ${studentData.length}`);
+  const fields = {};
+
+  for (const student of studentData) {
+    const info = student.split(',');
+    const firstname = info[0];
+    const field = info[3];
+    if(!fields[field]) {
+      fields[field] =[];
+    }
+    fields[field].push(firstname);
+  }
+  for (const field in fields) {
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      );
+  }
+}
+
+module.exports = countStudents;
